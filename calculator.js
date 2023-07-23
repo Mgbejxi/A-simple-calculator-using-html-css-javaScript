b5ccaa1ca9deed9232589144a249a08c41f41909
mgbejxi <mgbejiobassy@gmail.com>
2023-07-23 17:21:38 +0100

    this is my first commit

---
+++ calculator.js
@@ -0,0 +1,24 @@
+
+let result=document.getElementById("inputext");
+
+let calculate = (number) => {
+result.value += number; 
+}
+
+let Result = () => {
+    try{
+
+        result.value = eval(result.value)
+    }
+    catch(err) {
+        alert("Enter the valid input");
+    }
+}
+
+function clr() {
+    result.value ="";
+}
+
+function del() {
+    result.value = result.value.slice(0, -1);
+}
